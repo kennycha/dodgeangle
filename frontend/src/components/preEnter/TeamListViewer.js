9f607d1d835614e805084a94ffb7111f615ec697
kennycha <dudqn136@naver.com>
2020-09-17 00:21:59 +0900

    [Fix] Typo

--- frontend/src/components/preEnter/TeamListViewer.js
+++ frontend/src/components/preEnter/TeamListViewer.js
@@ -23,34 +23,6 @@ const TeamMateInfo = styled.div`
   font-weight: bold;
 `;
 
-// const PositionItemBlock = styled.div`
-//   height: 100px;
-//   display: flex;
-//   justify-content: flex-end;
-//   align-items: center;
-// `;
-
-// const PositionItem = styled.div`
-//   display: relative;
-// `;
-
-// const PositionOverlay = styled.div`
-//   height: 80px;
-//   width: 80px;
-//   border-radius: 100%;
-//   position: absolute;
-//   background-color: ${palette.black};
-//   opacity: 0.7;
-//   display: none;
-// `;
-
-// const PositionImg = styled.img`
-//   height: 80px;
-//   width: 80px;
-//   margin-right: 1rem;
-//   border-radius: 100%;
-// `;
-
 const TeamListItem = ({ teamMate, dragStart, dragEnd, onPositionClick }) => {
   return (
     <TeamListItemBlock
