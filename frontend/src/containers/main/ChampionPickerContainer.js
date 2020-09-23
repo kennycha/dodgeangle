fb753d72680458df96d38d26491c1a73a5f47453
kennycha <dudqn136@naver.com>
2020-09-23 22:47:34 +0900

    [Add] Champion Picker
    챔피언 검색 인풋 및 리스트 추가
    인풋 변화에 따라 아래 리스트 값이 변하도록 구현
    리스트 내 아이템 클릭 시 크기 변화 구현
    리스트 내 아이템에 챔피언 이름과 사진 추가
    리스트 높이 고정 및 넘치는 부분 스크롤 구현
    lib 내 모든 챔피언 정보를 담은 json 데이터 추가

--- frontend/src/containers/main/ChampionPickerContainer.js
+++ frontend/src/containers/main/ChampionPickerContainer.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import ChampionPicker from '../../components/main/ChampionPicker';
+import allchampions from '../../lib/allchampions';
 
 const ChampionPickerContainer = () => {
   // api를 통해 전체 챔피언을 가져온 후 가지고 있을 것
-  const allChampions = [];
+  const allChampions = allchampions;
   // 인풋 밸류에 따라서 allChampions 중 결과에 해당하는 챔피언들만 array에 담아서 가지고 있음
   const [resultChampions, setResultChampions] = useState(allChampions);
   // 챔피언 리스트 중에서 선택된 챔피언, 선택된 경우 소환사를 지정할 수 있도록 할 것
