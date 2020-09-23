b53a712f1ed43bc8e0a86afc63e6af574e71b391
jhj9109 <jhj91_09@naver.com>
2020-09-23 13:30:59 +0900

    [Update] AllyList - 소환사 div 생성 & teamMates 인자로 활용
    AllyListContainer.js
    - 인자로 teamMates 활용
    AllyList.js
    - AllyList : 아군 소환사 5명
    - AllyListItem
     - ChampionImg : 챔피언 선택전엔 포지션별 이미지 사용

--- frontend/src/components/main/AllyList.js
+++ frontend/src/components/main/AllyList.js
@@ -7,8 +7,64 @@ const AllyListBlock = styled.div`
   background: ${palette.orange[3]};
 `;
 
-const AllyList = () => {
-  return <AllyListBlock>AllyList</AllyListBlock>;
+const AllyListItemBlock = styled.div`
+  background: ${palette.indigo[5]};
+  margin: 0.5rem;
+  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const AllyInfo = styled.div`
+  margin-left: 1.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+const ChampionImg = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-right: 1rem;
+  border-radius: 100%;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+`;
+
+const AllyListItem = ({
+  teamMate
+}) => {
+  console.log("팀메이트 확인", teamMate)
+  return (
+    <AllyListItemBlock>
+      <AllyInfo>
+        <ChampionImg
+          // src={require(`../../img/champions/${teamMate?.name}.png`)}
+          src={require(`../../img/positions/${teamMate?.pos}.png`)}
+        />
+        소환사명&챔피언&포지션&승률&최근게임&챔피언성적
+      </AllyInfo>
+    </AllyListItemBlock>
+  )
+}
+
+const AllyList = ({
+  teamMates,
+}) => {
+  return (
+    <AllyListBlock>
+      {teamMates?.map((teamMate) => (
+        <AllyListItem
+          key={teamMate.id}
+          teamMate={teamMate}
+        />
+      ))}
+    </AllyListBlock>
+  );
 };
 
 export default AllyList;
--- frontend/src/containers/main/AllyListContainer.js
+++ frontend/src/containers/main/AllyListContainer.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import AllyList from '../../components/main/AllyList';
+import { useSelector } from 'react-redux';
+// import {
+//   changePosition,
+//   changeMe,
+//   switching,
+// } from '../../modules/teamMates';
 
 const AllyListContainer = () => {
-  return <AllyList />;
+  // const dispatch = useDispatch();
+  // const [dragged, setDragged] = useState();
+  // const [over, setOver] = useState();
+  const { teamMates } = useSelector(({ teamMates }) => ({
+    teamMates: teamMates.teamMates,
+  }));
+  console.log("팀메이츠확인", teamMates)
+  return (
+    <AllyList
+      teamMates={teamMates}
+    />
+  );
 };
 
 export default AllyListContainer;
