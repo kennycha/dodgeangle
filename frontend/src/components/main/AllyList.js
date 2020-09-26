10bb40a2e71912138fa62a314e23410051d9afae
jhj9109 <jhj91_09@naver.com>
2020-09-26 19:33:14 +0900

    Update AllyList.js 소환사블럭 구조 수정
    - AllyListItemBlock
     - 변동 X
     - flex, space-between, align-items: center
    - AllyInfoBlock
     - 신규 생성
     - AllyListItemBlock 하위 Div
      - AllyInfo
       - 마진 1.5 => 0.5rem으로 축소
       - display : 기본 => flex 변경
       - align-items: center 신규
        - ChampionImg
         - display : 기본 => block 변경

--- frontend/src/components/main/AllyList.js
+++ frontend/src/components/main/AllyList.js
@@ -18,11 +18,19 @@ const AllyListItemBlock = styled.div`
   align-items: center;
 `;
 
-const AllyInfo = styled.div`
-  margin-left: 1.5rem;
+const AllyInfoBlock = styled.div`
   font-size: 1.25rem;
   font-weight: bold;
   color: ${mainTheme.mainFontColor};
+`;
+
+const AllyInfo = styled.div`
+  margin-left: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: ${mainTheme.mainFontColor};
+  display: flex;
+  align-items: center;
 `;
 
 const ChampionImg = styled.img`
@@ -30,33 +38,44 @@ const ChampionImg = styled.img`
   width: 40px;
   margin-right: 1rem;
   border-radius: 100%;
-  user-select: none;
-  -moz-user-select: none;
-  -webkit-user-drag: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
+  display: block;
 `;
+
+const SummonerName = styled.div`
+  display: block;
+`
 
 const AllyListItem = ({ teamMate }) => {
   return (
     <AllyListItemBlock>
-      <AllyInfo>
-        {/* 벤한 캐릭터, 추후 삭제 => 다른 곳으로 이동할 필요성 */}
-        <ChampionImg
-          src={require(`../../img/${
-            teamMate.ban ? 'champions/' + teamMate.ban.image : 'question.png'
-          }`)}
-        />
-        {/* 선택한 position or 캐릭터 */}
-        <ChampionImg
-          src={require(`../../img/${
-            teamMate.pick
-              ? 'champions/' + teamMate.pick.image
-              : 'positions/' + teamMate.pos + '.png'
-          }`)}
-        />
-        {teamMate?.name}
-      </AllyInfo>
+      <AllyInfoBlock>
+        <AllyInfo>
+          {/* 벤한 캐릭터, 추후 삭제 => 다른 곳으로 이동할 필요성 */}
+          <ChampionImg
+            src={require(`../../img/${
+              teamMate.ban ? 'champions/' + teamMate.ban.image : 'question.png'
+            }`)}
+          />
+          {/* 선택한 position or 캐릭터 */}
+          <ChampionImg
+            src={require(`../../img/${
+              teamMate.pick
+                ? 'champions/' + teamMate.pick.image
+                : 'positions/' + teamMate.pos + '.png'
+            }`)}
+          />
+          <SummonerName>
+            {teamMate?.name}
+          </SummonerName>
+        </AllyInfo>
+      </AllyInfoBlock>
+      <AllyInfoBlock>
+        <AllyInfo>
+          <SummonerName>
+            임시 우측 공간
+          </SummonerName>
+        </AllyInfo>
+      </AllyInfoBlock>
     </AllyListItemBlock>
   );
 };
