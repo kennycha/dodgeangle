b67c299ea456793543f93a53bb9d81c40796e4b5
jhj9109 <jhj91_09@naver.com>
2020-09-28 00:57:25 +0900

    Update EnemyListContainers 로컬스토리지관련 useEffect 추가
    EnemyLIstContainers.js
    - import
     - confirmEnemies : 로컬스토리지 값을 Redux로 업데이트할때 필요
     - useDispatch : 로컬스토리지 값을 Redux로 업데이트할때 필요
     - useEffect : 로컬스토리지와 연동하는 로직 구현에 필요
    - useEffect
     - enemies 존재 : 로컬스토리지에 업데이트
     - enemies 미존재 : 초기화 or 로컬스토리지값을 가져오기

--- frontend/src/containers/main/EnemyListContainer.js
+++ frontend/src/containers/main/EnemyListContainer.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import EnemyList from '../../components/main/EnemyList';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { confirmEnemies } from '../../modules/enemies'
 
 const EnemyListContainer = () => {
+  const dispatch = useDispatch();
+
   const { enemies } = useSelector(({ enemies }) => ({
     enemies: enemies.enemies,
   }));
+
+  // 이 공간엔 AllyList의 모스트 챔피언 => EnemyList의 카운터 챔피언으로 대칭
+  
+  useEffect(() => {
+    // enemies 변화되었거나 첫 렌더링일때 useEffect는 실행
+    if (enemies) {
+      // enemies !== null 일때 => enemies 로컬스토리지에 업데이트
+      localStorage.setItem(
+        "enemies",
+        JSON.stringify(
+          enemies.map(enemy => ({
+            id: enemy.id,
+            ban: enemy.ban,
+            pick: enemy.pick,
+          }))
+        )
+      );
+    } else {
+      // enemies === null 일때
+      if (localStorage.getItem("enemies")) {
+        // 로컬스토리 값이 존재 => 로컬스토리지 값을 Redux에 반영하기
+        dispatch(confirmEnemies(JSON.parse(localStorage.getItem("enemies"))))
+      } else {
+        // enemies가 null 값 === 초기 생성이후, null값으로 변경된 상태에서 Redux에 남아 있는 상태
+        // => 초기화 한다.
+        dispatch(confirmEnemies(null))
+      }
+    }
+  },
+  [enemies, dispatch]
+  );
   return <EnemyList enemies={enemies} />;
 };
 
