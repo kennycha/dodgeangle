1e55e297aebad941b1500ddab15754672515d91d
jhj9109 <jhj91_09@naver.com>
2020-09-30 03:28:15 +0900

    Update ListContainer 모스트 & 예상 & 카운터 챔 관련 수정
    공통
    - 목업데이터 : random 활용하는 코드로 수정
    
    EnemyListContainer
    - 새로운 useEffect 작성
     - 예상 or 카운터 챔피언을 목업데이터로 넣는 메서드

--- frontend/src/containers/main/AllyListContainer.js
+++ frontend/src/containers/main/AllyListContainer.js
@@ -3,6 +3,7 @@ import AllyList from '../../components/main/AllyList';
 import { useSelector, useDispatch } from 'react-redux';
 import { setMostChampions, confirmTeamMates } from '../../modules/teamMates';
 import { useHistory } from 'react-router-dom';
+import allchampions from '../../lib/allchampions';
 
 const AllyListContainer = () => {
   const dispatch = useDispatch();
@@ -17,71 +18,25 @@ const AllyListContainer = () => {
 
   useEffect(() => {
     // 아래는 임시코드, 실제론 API 요청해서 받기
-    if (teamMates && !teamMates.mostChampions) {
+
+    if (teamMates && teamMates[0]?.mostChampions == null) {
       // PreEnter에서 MainPage로 처음 넘어오는 상황에서 작동
       // === teamMates는 존재하는데, 모스트 챔피언은 없으면
       // => 모스트 챔피언 요청
-      const mostChampionsArray = [
-        {
-          id: 0,
-          champions: [
-            { id: 111, name: '카밀', img: 'Camille.png', winRate: 70 },
-            { id: 111, name: '아트록스', img: 'Aatrox.png', winRate: 80 },
-            { id: 111, name: '아무무', img: 'Amumu.png', winRate: 90 },
-          ],
-          winRates: [45, 25, 35],
-        },
-        {
-          id: 1,
-          champions: [
-            { id: 111, name: '티모', img: 'Teemo.png', winRate: 11 },
-            { id: 111, name: '애쉬', img: 'Ashe.png', winRate: 22 },
-            { id: 111, name: '카밀', img: 'Camille.png', winRate: 33 },
-          ],
-          winRates: [45, 25, 35],
-        },
-        {
-          id: 2,
-          champions: [
-            { id: 111, name: '아지르', img: 'Azir.png', winRate: 44 },
-            { id: 111, name: '티모', img: 'Teemo.png', winRate: 55 },
-            { id: 111, name: '애쉬', img: 'Ashe.png', winRate: 66 },
-          ],
-          winRates: [70, 27, 37],
-        },
-        {
-          id: 3,
-          champions: [
-            { id: 111, name: '애쉬', img: 'Ashe.png', winRate: 77 },
-            { id: 111, name: '아지르', img: 'Azir.png', winRate: 88 },
-            { id: 111, name: '티모', img: 'Teemo.png', winRate: 99 },
-          ],
-          winRates: [91, 29, 93],
-        },
-        {
-          id: 4,
-          champions: [
-            { id: 111, name: '아트록스', img: 'Aatrox.png', winRate: 12 },
-            { id: 111, name: '아무무', img: 'Amumu.png', winRate: 23 },
-            { id: 111, name: '티모', img: 'Teemo.png', winRate: 34 },
-          ],
-          winRates: [55, 44, 33],
-        },
-      ];
-      mostChampionsArray.forEach((arr) => {
+      teamMates.forEach((teamMate) => {
+        let newMostChampions = [0,1,2].map(() => ({
+          ...allchampions[Math.floor((Math.random()*allchampions.length))],
+          winRate: Math.floor((Math.random()*100)),
+        }))
         dispatch(
           setMostChampions({
-            id: arr.id,
-            champions: arr.champions,
-            // 기존 샘플 코드
-            // champions: [0,1,2].map(idx =>
-            //   [arr.champions[idx], arr.winRates[idx]]
-            // )
+            id: teamMate.id,
+            champions: newMostChampions,
           }),
         );
       });
     }
-  }, [dispatch]);
+  }, [teamMates, dispatch]);
 
   useEffect(() => {
     // teamMates가 변화되었거나 첫 렌더링일때 useEffect는 실행
