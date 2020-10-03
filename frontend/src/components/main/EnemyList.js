b5feca54dcd739de49bc5aec8a1c715f3e6d6c29
jhj <janghj18@gmail.com>
2020-10-03 14:58:21 +0900

    Hotfix EnemyList | 불필요한 텍스트 삭제 |
    - 카운터 챔피언 공간에 "빈공간"으로 잡아두었던것 삭제

--- frontend/src/components/main/EnemyList.js
+++ frontend/src/components/main/EnemyList.js
@@ -118,7 +118,7 @@ const EnemyListItem = ({ enemy }) => {
                 <ChampionWinRate>{champion.winRate}</ChampionWinRate>
               </CounterOrExpetedChampion>
             ))
-          ) : enemy.expectedChampions !== null ? (
+          ) : enemy.expectedChampions !== null && (
             enemy.expectedChampions.map((champion) => (
               <CounterOrExpetedChampion key={enemy.expectedChampions.indexOf(champion)}>
                 <ChampionMiniImg
@@ -127,8 +127,6 @@ const EnemyListItem = ({ enemy }) => {
                 <ChampionWinRate>{champion.winRate}</ChampionWinRate>
               </CounterOrExpetedChampion>
             ))
-          ) : (
-            <SummonerName>빈공간</SummonerName>
           )}
         </CounterOrExpetedChampionsBlock>
       </EnemyInfoBlock>
