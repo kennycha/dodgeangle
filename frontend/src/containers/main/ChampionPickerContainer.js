d2bb3e2ccdf2e78af1ad0e23a1572ba2380a687c
kennycha <dudqn136@naver.com>
2020-10-03 22:22:01 +0900

    [Update] MainPage - phase 관련
    - phase 에 따라 챔피언 리스트 배경 색이 바뀌도록 구현
    - phase 변경하는 버튼을 챔피언 리스트 내 absolute position 으로 구현
    - 네이게이션의 new 클릭 시 phase 초기화
    - preenterpage의 팀원 재설정 클릭 시 phase 초기화 등

--- frontend/src/containers/main/ChampionPickerContainer.js
+++ frontend/src/containers/main/ChampionPickerContainer.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ChampionPicker from '../../components/main/ChampionPicker';
 import allchampions from '../../lib/allchampions';
+import { changePhase } from '../../modules/phase';
 
 const ChampionPickerContainer = () => {
   // api를 통해 전체 챔피언을 정렬해서 가져온 후 가지고 있을 것
@@ -10,9 +12,25 @@ const ChampionPickerContainer = () => {
   // 챔피언 리스트 중에서 선택된 챔피언, 선택된 경우 소환사를 지정할 수 있도록 할 것
   const [selectedChampion, setSelectedChampion] = useState();
 
+  const { phase } = useSelector(({ phase }) => ({ phase: phase.phase }));
+  const dispatch = useDispatch();
+  const onPhaseBtnClick = () => {
+    if (phase !== 'complete') {
+      const ok = window.confirm(
+        `확인 시 ${
+          phase === 'ban' ? '벤 정보' : '픽 정보'
+        } 변경이 불가합니다. 계속 진행하시겠습니까?`,
+      );
+      if (ok) {
+        dispatch(changePhase({ currentPhase: phase }));
+      }
+    }
+  };
+
   useEffect(() => {
     // 여기서 전체 챔피언 가져올 것
   }, []); // 첫 render 시에만 {} 안의 내용을 실행
+
   return (
     <ChampionPicker
       allChampions={allChampions}
@@ -20,6 +38,8 @@ const ChampionPickerContainer = () => {
       setResultChampions={setResultChampions}
       selectedChampion={selectedChampion}
       setSelectedChampion={setSelectedChampion}
+      phase={phase}
+      onPhaseBtnClick={onPhaseBtnClick}
     />
   );
 };
