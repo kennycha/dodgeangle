ec19418ad616668fd3187d3556c5c78fa63ae22e
jhj9109 <jhj91_09@naver.com>
2020-10-05 19:25:39 +0900

    Update | useEffect 활용한 로컬스토리지 가져오기 일괄 삭제 |
    로컬스토리지값을 Redux로 동기화하는 코드 구현 위치가
    useEffect에서 initialState로 변화함에 따라 삭제
    (단, Redux값을 로컬스토리지로 동기화하는 useEffect 코드는 유지)

--- frontend/src/containers/preEnter/PreEnterButtonContainer.js
+++ frontend/src/containers/preEnter/PreEnterButtonContainer.js
@@ -27,14 +27,6 @@ const PreEnterButtonContainer = () => {
           })),
         ),
       );
-    } else {
-      // teamMates === null 일때
-      if (localStorage.getItem('teamMates')) {
-        // 로컬스토리 값이 존재 => 로컬스토리지 값을 Redux에 반영하기
-        dispatch(
-          confirmTeamMates(JSON.parse(localStorage.getItem('teamMates'))),
-        );
-      }
     }
   }, [teamMates, dispatch]);
   return (
