063f6fc9937147fbed688757373a6033e6d757dd
yuueuni <yuueuni@gmail.com>
2020-10-06 01:43:53 +0900

    [Add] 챔피언별 룬 정보 #S03P23A504-87 /champion_id/ api 설정 및 DB 입력

--- backend/champion/models.py
+++ backend/champion/models.py
@@ -6,3 +6,16 @@ class Champion(models.Model):
     image = models.CharField(max_length=200)
     pos = models.CharField(max_length=200)
     counter = models.CharField(max_length=200)
+
+class Rune(models.Model):
+    primary = models.IntegerField()
+    primary1 = models.IntegerField()
+    primary2 = models.IntegerField()
+    primary3 = models.IntegerField()
+    primary4 = models.IntegerField()
+    sub = models.IntegerField()
+    sub1 = models.IntegerField()
+    sub2 = models.IntegerField()
+    stat1 = models.IntegerField()
+    stat2 = models.IntegerField()
+    stat3 = models.IntegerField()
--- backend/champion/serializers.py
+++ backend/champion/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Champion
+from .models import Champion, Rune
 
 
 class ChampionSerializer(serializers.ModelSerializer):
@@ -10,3 +10,12 @@ class ChampionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Champion
         fields = ('id', 'name', 'image', 'counter', 'pos',)
+
+
+class RuneSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Rune
+        fields = ('primary', 'primary1', 'primary2', 'primary3', 'primary4', 'sub', 'sub1', 'sub2', 'stat1', 'stat2', 'stat3',)
+
+    
