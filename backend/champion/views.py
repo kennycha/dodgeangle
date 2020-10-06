49b39d446a06e0971a5354c36915c5225759242c
yuueuni <yuueuni@gmail.com>
2020-10-06 14:31:03 +0900

    [Add] 추천 챔피언 API, champion DB 수정

--- backend/champion/views.py
+++ backend/champion/views.py
@@ -3,18 +3,11 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .models import Champion, Rune
-from .serializers import ChampionSerializer, RuneSerializer
+from .models import Champion
+from .serializers import ChampionSerializer
 
 @api_view(['GET'])
 def get_all_champion(request):
     champion = Champion.objects.all()
     serializer = ChampionSerializer(champion, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def get_champion_rune(request, champion_id):
-    rune = Rune.objects.get(id=champion_id)
-    serializer = RuneSerializer(rune)
     return Response(serializer.data)
